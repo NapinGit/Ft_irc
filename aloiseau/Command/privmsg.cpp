31bf2b6ddab89b00f5cd49f728d5b43b8817bf61
Thibault JAHJAH-OUEIS <toueis@e3r10p17.clusters.42paris.fr>
2022-07-08 12:59:44 +0200

    push

--- aloiseau/Command/privmsg.cpp
+++ aloiseau/Command/privmsg.cpp
@@ -2,28 +2,21 @@
 
 void privmsg_cmd(Server *serv, Client *cli, std::string arg)
 {
-    std::istringstream      args;
-    std::string             name;
-    std::string             msg;
-
-    std::map<std::string, Channel *>::iterator                                          itchan;
-    std::map<int, Client *>::iterator                                                   itclient;
-    std::map<int, Client *>::iterator                                                   itcliente;
+    std::istringstream                          args;
+    std::string                                 name;
+    std::string                                 msg;
+    std::map<std::string, Channel *>::iterator  itchan;
+    std::map<int, Client *>::iterator           itclient;
+    std::map<int, Client *>::iterator           itcliente;
 
     args.str(arg);
     std::getline(args, name, ' ');
-    //std::cout << name << std::endl;
-    
-    //message to channel 
-    //else: message to client
     if (name.c_str()[0] == '#')
     {
         name.erase(0, 1);
-        //cherche si le cannal existe
         itchan = serv->_channels.find(name);
         std::getline(args, msg);
         msg.erase(0, 1);
-        //envoi le message a tout le cannal s'il existe
         if (itchan != serv->_channels.end())
             itchan->second->send_to_chan(cli, msg);
     }
@@ -31,17 +24,14 @@ void privmsg_cmd(Server *serv, Client *cli, std::string arg)
     {
         itclient = serv->clients.begin();
         itcliente = serv->clients.end();
-        //cherche le client sil existe 
         while ( itclient != itcliente && itclient->second->get_nickname() != name )
             itclient++;
-        //sil nexiste pas, RPL ERR PRIVMSG ??
         if (itclient == itcliente)
         {
             std::string reply = "401 " + cli->get_nickname() /*+ " :No such nick/channel"*/ ;
             cli->send_to_client(reply);
             return ;
         }
-        //envoi du message au client destinataire (itclient)
         std::getline(args, msg);
         std::string str;
         str = ":" + cli->get_nickname() + "!" + cli->get_username() + "@" + cli->get_hostname() + " PRIVMSG " + itclient->second->get_nickname() + " " + msg + "\r\n";
